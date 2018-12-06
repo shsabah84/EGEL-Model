ea6d26d205a06df6a62ec087a9701ae0cd08bd91
shsabah84 <45658992+shsabah84@users.noreply.github.com>
2018-12-06 13:13:34 +0000

    Update run.py

--- run.py
+++ run.py
@@ -5,8 +5,8 @@ model = EGEL.Model(embedding_size=50, learning_rate=0.5, batch_size=1024, scalin
 
 #input data files
 Txt_file = 'textual_Features.txt' #the textual data file must be in the form of  “region_id  term_id  value”
-NF_file = 'Numerical_Features.txt'#the numerical data file must be in the form of  “region_id  feature_id  value”
-Cat_file = 'Categorical_Features.txt'#the categorical data file must be in the form of “region_id  feature_id ”
+NF_file = 'Numerical_Features.txt' #the numerical data file must be in the form of  “region_id  feature_id  value”
+Cat_file = 'Categorical_Features.txt' #the categorical data file must be in the form of “region_id  feature_id ”
 
     
 region_len=200000 #number of regions or entities
@@ -15,7 +15,7 @@ cat_len=180 #number of categories
 vocab_len=100000 #number of terms in the corpus
 
 model.fit_to_corpus(region_len,NF_len+cat_len+vocab_len) #context length = NF_len+cat_len+vocab_len
-num_epochs=3
+num_epochs=30 #number of iterations
 model.train(num_epochs,Txt_file,NF_file,Cat_file)
 
-
+#the embedding vectors will be saved in EGEL.txt file which contains the vectors for all the regions each at a line
