3f8351bd095c324b7d6b70952b235ea1c019671e
shsabah84 <45658992+shsabah84@users.noreply.github.com>
2018-12-06 13:12:00 +0000

    Update run.py

--- run.py
+++ run.py
@@ -9,11 +9,12 @@ NF_file = 'Numerical_Features.txt'#the numerical data file must be in the form o
 Cat_file = 'Categorical_Features.txt'#the categorical data file must be in the form of “region_id  feature_id ”
 
     
-region_len=200000#number of regions or entities
-NF_len=4#number of numerical features
-cat_len=180#number of categories
-vocab_len=100000#number of terms in the corpus
-model.fit_to_corpus(region_len,NF_len+cat_len+vocab_len)
+region_len=200000 #number of regions or entities
+NF_len=4 #number of numerical features
+cat_len=180 #number of categories
+vocab_len=100000 #number of terms in the corpus
+
+model.fit_to_corpus(region_len,NF_len+cat_len+vocab_len) #context length = NF_len+cat_len+vocab_len
 num_epochs=3
 model.train(num_epochs,Txt_file,NF_file,Cat_file)
 
